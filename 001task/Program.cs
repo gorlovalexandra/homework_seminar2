452a65a894354313a83c70f3c61f8df2d4f88633
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-22 17:37:16 +0300

    add 001task

---
+++ 001task/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 1. Напишите метод, который выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа. */
+
+int Random(int a, int b)
+{
+    int number = new Random().Next(a, b);
+    return number;
+}
+
+int Max(int num)
+{
+    int num1 = num / 10;
+    int num2 = num % 10;
+    int max = 0;
+
+    if (num1 > num2) max = num1;
+    if (num1 < num2) max = num2;
+
+    return max;
+}
+
+int N = Random(10, 100);
+Console.WriteLine(N);
+
+int max = Max(N);
+Console.WriteLine(max);
