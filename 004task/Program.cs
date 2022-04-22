eb903e2e6c440c6ed787c50af4a9b057bee11da5
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-22 18:38:39 +0300

    add 004task

---
+++ 004task/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 4. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. */
+
+void Number(int N)
+{
+    if (N % 7 == 0 && N % 23 == 0)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+
+Number(14);
+Console.WriteLine();
