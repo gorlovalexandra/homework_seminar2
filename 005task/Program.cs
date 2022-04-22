a655dc076a0b2887b3d7d3aeb7264888bcaff36f
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-22 20:33:58 +0300

    add 005task

---
+++ 005task/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 5. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого. */
+
+void Square(int a, int b)
+{
+    if (a == b * b || b == a * a)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+
+Square(8, 9);
+Console.WriteLine();
