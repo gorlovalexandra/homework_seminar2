61313d091ee2f28517c8f4e1831a1857e907cd31
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-22 18:27:30 +0300

    add 003task

---
+++ 003task/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 3. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно первому числу, то программа выводит остаток от деления. */
+
+void Method(int numA, int numB)
+{
+    if (numB % numA == 0)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        int remains = numB / numA;
+        Console.WriteLine("no, remains " + remains);
+    }
+}
+
+Method(5, 36);
+Console.WriteLine();
