a946c71a871ea251ec2c909256f00e49f02c85c6
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-23 14:03:16 +0300

    add 008task

---
+++ 008task/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 8. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. */
+
+void WeekDay(int N)
+{
+    int Sat = 6;
+    int Sun = 7;
+
+    if (N < Sat)
+    {
+        Console.WriteLine("NO");
+    }
+    if (N > Sun)
+    {
+        Console.WriteLine("No weekday with this number");
+    }
+    if (N == Sat || N == Sun)
+    {
+        Console.WriteLine("YES");
+    }
+}
+
+WeekDay(10);
+Console.WriteLine();
